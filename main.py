8125e1ffb8c9fe752cc423966f9f8de849c48c62
yousef issa <yousef@u.northwestern.edu>
2017-05-03 13:47:50 -0500

    get 2cap balance
    Gets your 2Captcha balance.

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import requests
 import selenium 
 import time
 import threading 
+from dropconfig import *
+
+# displays 2captcha balance
+def captcha_balance(api_token):
+	if api_token == '':
+		print('No captcha token present. Check your config file.')
+	else:
+		balance = requests.get("http://2captcha.com/res.php?key={}&action=getbalance".format(api_token)).text
+		print('Your 2captcha balance is, ', balance)
 
 # imports proxies
 try:
@@ -11,5 +20,3 @@ try:
 except IOError:
 	print('Error importing proxy file.')
 	exit()
-
-
