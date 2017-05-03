5ac83fbfb1b3f8fd52ef5636fe89d6dc9ea89817
yousef issa <yousef@u.northwestern.edu>
2017-05-03 13:11:23 -0500

    Initial

---
+++ dropconfig.py
@@ -0,0 +1,12 @@
+# This information should be edited 
+
+# put the adidas release link here
+release_link = ''
+
+# put the number of tasks/proxies that you want to run
+num_tasks = ''
+
+# put 2captcha api token here. leave blank if unused.
+captcha_api = ''
+
+
