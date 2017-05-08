7417be2315820de85ee3bca2591e6497418ddd77
yousef issa <yousef@u.northwestern.edu>
2017-05-08 13:22:14 -0500

    Adding menu

--- main.py
+++ main.py
@@ -5,6 +5,35 @@ import time
 import threading 
 from dropconfig import *
 
+# main menu
+def menu():
+	    print('''
+Please read the readme on Github before using this bot.
+
+This is a bot that allows you to do a few things: 
+
+	1. Load proxies
+	2. Get captcha balance.
+    3. Quit. 
+    ''')
+
+	userChoice = input('Enter the action that you want to take: ')
+	choices {
+	1: load_proxies,
+	2: captcha_balance,
+	3: quit 
+	}
+
+	try:
+		choices[int(userChoice)]()
+	except:
+        print('Input not recognized. You probably did not enter a number. \n'
+              'The program will restart. \n')
+        menu()
+    finally:
+        Continue()
+
+
 # displays 2captcha balance
 def captcha_balance(api_token):
 	if api_token == '':
